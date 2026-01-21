b570561bcce3e978f21e088dfedfd5355e9643fa
Austin Chase <10963252@uvu.edu>
2026-01-21 10:17:18 -0700

    finished step: 1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+WorkWithIntegers();
+
+void WorkWithIntegers()
+{
+    // Example 1 
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+
+    Console.WriteLine(c);
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+
+    /// Addition, Subtraction, and Multiplication 
+    c = a + b - 12 * 17;
+    Console.WriteLine(c); 
+}
